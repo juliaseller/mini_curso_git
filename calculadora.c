66955c0d5d7063681ca39709a156ba6251817629
Julia Sa Eller <juliasaeller@40o.fct.unesp.br>
2024-10-04 10:25:07 -0300

    Menu

--- calculadora.c
+++ calculadora.c
@@ -43,26 +43,26 @@ int main(){
                 scanf("%lf", &n1);
                 printf("\nDigite um outro numero: ");
                 scanf("%lf", &n2);
-                printf("\nA adicao de %f e %f resulta em: ", adicao(n1, n2));
+                printf("\nA adicao de %.2f e %.2f resulta em: %.2f", n1, n2, adicao(n1, n2));
                 break;
             case 2:
                 printf("\nDigite um numero: ");
                 scanf("%lf", &n1);
                 printf("\nDigite um outro numero: ");
                 scanf("%lf", &n2);
-                printf("\nA subtracao de %f e %f resulta em: ", subtracao(n1, n2));
+                printf("\nA subtracao de %.2f e %.2f resulta em: %.2f", n1, n2, subtracao(n1, n2));
                 break;
             case 3:
                 printf("\nDigite um numero: ");
                 scanf("%lf", &n1);
                 printf("\nDigite um outro numero: ");
                 scanf("%lf", &n2);
-                printf("\nA potenciacao de %f e %f resulta em: ", potenciacao(n1, n2));
+                printf("\nA potenciacao de %.2f e %.2f resulta em: %.2f", n1, n2, potenciacao(n1, n2));
                 break;
             case 4:
                 printf("\nDigite um numero: ");
                 scanf("%lf", &n1);
-                printf("\nA raiz quadrada de %f resulta em: ", raiz_quadrada(n1));
+                printf("\nA raiz quadrada de %.2f resulta em: %.2f", n1, raiz_quadrada(n1));
                 break;
             default:
                 printf("\nOperacao invalida!\n");
