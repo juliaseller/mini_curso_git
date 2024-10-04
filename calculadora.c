75a8a8342cdcad7bfe97d420c9df505255281554
Julia Sa Eller <juliasaeller@40o.fct.unesp.br>
2024-10-04 09:29:17 -0300

    operacao de potenciacao

--- calculadora.c
+++ calculadora.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
+#include<math.h>
 
-int adicao(int a, int b){
-    int soma;
+double adicao(double a, double b){
+    double soma;
     soma = a + b;
     return soma;
 }
 
-int subtracao(int a, int b){
-    int sub;
+double subtracao(double a, double b){
+    double sub;
     sub = a - b;
     return sub;
+}
+
+double potenciacao(double a, double b){
+    double potencia;
+    potencia = pow(a, b);
+    return potencia;
 }
 
 
