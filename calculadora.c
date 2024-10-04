879278b97bc16b0b9276f0d546ce936883753a66
Julia Sa Eller <juliasaeller@40o.fct.unesp.br>
2024-10-04 09:59:40 -0300

    Menu e operacoes

--- calculadora.c
+++ calculadora.c
@@ -27,7 +27,48 @@ double raiz_quadrada(double a){
 
 
 int main(){
+    double n1, n2;
+    int op;
 
+    while(op != 0){
+        printf("\n##### MENU #####\n0 - Sair\n1 - Adicao\n2 - Subtracao\n3 - Potenciacao\n4 - Raiz quadrada\n");
+        printf("\nDigite sua opcao: ");
+        scanf("%d", &op);
+
+        switch(op){
+            case 0:
+                break;
+            case 1:
+                printf("\nDigite um numero: ");
+                scanf("%lf", &n1);
+                printf("\nDigite um outro numero: ");
+                scanf("%lf", &n2);
+                printf("\nA adicao de %f e %f resulta em: ", adicao(n1, n2));
+                break;
+            case 2:
+                printf("\nDigite um numero: ");
+                scanf("%lf", &n1);
+                printf("\nDigite um outro numero: ");
+                scanf("%lf", &n2);
+                printf("\nA subtracao de %f e %f resulta em: ", subtracao(n1, n2));
+                break;
+            case 3:
+                printf("\nDigite um numero: ");
+                scanf("%lf", &n1);
+                printf("\nDigite um outro numero: ");
+                scanf("%lf", &n2);
+                printf("\nA potenciacao de %f e %f resulta em: ", potenciacao(n1, n2));
+                break;
+            case 4:
+                printf("\nDigite um numero: ");
+                scanf("%lf", &n1);
+                printf("\nA raiz quadrada de %f resulta em: ", raiz_quadrada(n1));
+                break;
+            default:
+                printf("\nOperacao invalida!\n");
+                break;
+        }
+    }
 
 
     return 0;
